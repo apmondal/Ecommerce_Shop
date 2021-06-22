9948a1a3ae2c0cf258ba98701790538cb9a5117f
phoenix0010 <apmondal0010@gmail.com>
2021-06-22 20:07:44 +0530

    Add sliding in home page

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -6,6 +6,7 @@ import {
     updateProduct,
     createProduct,
     createProductReview,
+    getTopProducts,
 } from '../controllers/productController.js'
 import { admin, protect } from '../middleware/authMiddleware.js'
 
@@ -15,6 +16,8 @@ const router = express.Router()
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 
 router.route('/:id/reviews').post(protect, createProductReview)
+
+router.get('/top', getTopProducts)
 
 // @route   GET /api/products/:id
 router
